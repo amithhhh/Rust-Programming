84b2497e19c7b3c56a316f53d0173164f731a9fc
amithhhhh <amithekk@gmail.com>
2026-02-08 11:41:03 +0630

    additonal codes

---
+++ youtube-tutorial/08-02-2026/check.rs
@@ -0,0 +1,8 @@
+fn main() {
+	let a = 99;
+	if a > 99 {
+		println!("Big number");
+	} else {
+		println!("Small number");
+	}
+}
---
+++ youtube-tutorial/08-02-2026/macro.rs
@@ -0,0 +1,11 @@
+fn main() {
+	let life = 42;
+	println!("Hello!");
+	println!("{:?}", life);
+	println!("{life:?}");
+	println!("{life}");
+	let nums = vec![1, 2, 3];
+
+	println!("{nums:?}");
+
+}
