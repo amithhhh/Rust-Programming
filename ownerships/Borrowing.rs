e0ebde73f2157f475aec295d26933cbe01df5f89
amithhhhh <amithajith7025@gmail.com>
2026-02-21 19:40:06 +0630

    Auto commit: ownerships/Borrowing.rs

---
+++ ownerships/Borrowing.rs
@@ -0,0 +1,9 @@
+fn main() {
+	let s1 = String::from("Hola, Madridista");
+	let len = calculate_length(&s1);
+	println!("The length of '{s1}' is {len}.");
+}
+
+fn calculate_length(s: &String) -> usize {
+	s.len()
+}
