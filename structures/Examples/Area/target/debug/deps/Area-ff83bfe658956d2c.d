1bc0cecd9c57f4addcef3be117d85e44f2bf06b7
amithhhhh <amithajith7025@gmail.com>
2026-02-21 19:40:14 +0630

    Auto commit: structures/Examples/Area/target/debug/deps/Area-ff83bfe658956d2c.d

---
+++ structures/Examples/Area/target/debug/deps/Area-ff83bfe658956d2c.d
@@ -0,0 +1,5 @@
+/mnt/stone/Rust-Programming-Language/structures/Examples/Area/target/debug/deps/Area-ff83bfe658956d2c.d: src/main.rs
+
+/mnt/stone/Rust-Programming-Language/structures/Examples/Area/target/debug/deps/Area-ff83bfe658956d2c: src/main.rs
+
+src/main.rs:
