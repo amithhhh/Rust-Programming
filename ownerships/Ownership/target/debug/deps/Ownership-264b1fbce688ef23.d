0bc9ca4c9fc26b7920625abbdf412f16db564133
amithhhhh <amithajith7025@gmail.com>
2026-02-21 19:40:08 +0630

    Auto commit: ownerships/Ownership/target/debug/deps/Ownership-264b1fbce688ef23.d

---
+++ ownerships/Ownership/target/debug/deps/Ownership-264b1fbce688ef23.d
@@ -0,0 +1,5 @@
+/mnt/stone/Rust-Programming-Language/ownerships/Ownership/target/debug/deps/Ownership-264b1fbce688ef23.d: src/main.rs
+
+/mnt/stone/Rust-Programming-Language/ownerships/Ownership/target/debug/deps/Ownership-264b1fbce688ef23: src/main.rs
+
+src/main.rs:
